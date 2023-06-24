494cb374f477ddbd10cde9569f44046096c036e9
Matheus Oliveira <matheusoliveira.2018@alunos.utfpr.edu.br>
2023-06-23 22:12:25 -0300

    Criando introdução do jogo e menu
    ainda falta adicionar musica e feitos sonoros.
    O menu ainda falta adicionar mais botões, efeitos sonoros, e animar os botoes.

---
+++ Assets/Scripts/IntroductionController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class IntroductionController : MonoBehaviour
+{
+    public GameObject firstScene;
+    public GameObject secondScene;
+    public GameObject menu;
+    private float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = 0;
+        firstScene.SetActive(true);
+        secondScene.SetActive(false);
+        menu.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(time>=2f && time<=3f){
+            firstScene.SetActive(false);
+            secondScene.SetActive(true);
+        }
+        if(time>3f){
+            firstScene.SetActive(false);
+            secondScene.SetActive(false);
+            menu.SetActive(true);
+        }
+        time += Time.deltaTime;
+    }
+
+    
+    public void StartGame(){
+        SceneManager.LoadScene("JorgeLands");
+    }
+}
+
