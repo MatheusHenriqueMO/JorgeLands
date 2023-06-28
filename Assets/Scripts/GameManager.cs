a670926b82345e4b4c65a6cba0e61df9cc289da8
Jakeline Andrade <jakelineandradeds@gmail.com>
2023-06-28 15:28:07 -0300

    DropItem

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+    public ItemManager itemManager;
 
     private void Awake()
     {
@@ -16,5 +17,8 @@ public class GameManager : MonoBehaviour
         {
             instance = this;
         }
+        DontDestroyOnLoad(this.gameObject);
+
+        itemManager = GetComponent<ItemManager>();
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,5 +10,14 @@ public class Player : MonoBehaviour
     {
         inventory = new Inventory(12);
     }
-    // public int ax = 0;
+    public void DropItem(Collectable item)
+    {
+        Vector2 spawnLocation = transform.position;
+
+        Vector2 spawnOffset = Random.insideUnitCircle * 0.6f;
+
+        Collectable droppeditem = Instantiate(item, spawnLocation + spawnOffset, Quaternion.identity);
+
+        droppeditem.rb2d.AddForce(spawnOffset * 0.5f, ForceMode2D.Impulse);
+    }
 }
