b1cb5f66d86a6c4e7b54b1a25ef2fc9261bc5a93
Jakeline Andrade <jakelineandradeds@gmail.com>
2023-06-29 15:21:47 -0300

    drop

--- Assets/Scripts/UI/Inventory_UI.cs
+++ Assets/Scripts/UI/Inventory_UI.cs
@@ -1,12 +1,24 @@
+using System.Numerics;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Vector2 = UnityEngine.Vector2;
 
 public class Inventory_UI : MonoBehaviour
 {
     public GameObject inventoryPanel;
     public Player player;
     public List<Slot_UI> slots = new List<Slot_UI>();
+
+    [SerializeField] private Canvas canvas; 
+    private Image draggedIcon;
+    private Slot_UI draggedSlot;
+
+    private void Awake()
+    {
+        canvas = FindObjectOfType<Canvas>();
+    }
     void Start()
     {
         inventoryPanel.SetActive(false);
@@ -58,6 +70,46 @@ public class Inventory_UI : MonoBehaviour
             player.inventory.Remove(slotID);
             Refresh();
         }
+    }
+    public void SlotBeginBrag(Slot_UI slot)
+    {
+        draggedSlot = slot;
+        draggedIcon = Instantiate(draggedSlot.itemIcon);
+        draggedIcon.transform.SetParent(canvas.transform);
+        draggedIcon.raycastTarget = false;
+        draggedIcon.rectTransform.sizeDelta = new Vector2(50, 50);
         
+        MovetoMousePosition(draggedIcon.gameObject);
+        Debug.Log("Start Drag: " + draggedSlot.name);
+    }
+
+    public void SlotDrag()
+    {
+        MovetoMousePosition(draggedIcon.gameObject);
+
+        Debug.Log("Dragging: " + draggedSlot.name);
+    }
+    public void SlotEndDrag()
+    {
+        Debug.Log("Done Dragging: " + draggedSlot.name);
+    }
+
+    public void SlotDrop(Slot_UI slot)
+    {
+        Debug.Log("Dropped " + draggedSlot.name + " on " + slot.name);
+    }
+
+    private void MovetoMousePosition(GameObject toMove)
+    {
+        if(canvas != null)
+        {
+            Vector2 position;
+
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.transform as RectTransform, 
+            Input.mousePosition, null, out position);
+
+            toMove.transform.position = canvas.transform.TransformPoint(position);
+
+        }
     }
 }
